5a60a2958fc824b03659176cef4d8c44984dda45
ashish_mobitplus <ashish@intellikart.in>
2021-11-25 07:17:21 +0530

    search by image

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const search = require("../controller/search.controller");
 
 const express = require("express");
 const router = express.Router();
-let upload = require("../config/multer.config.js");
 
 const cors = require("cors");
 const app = express();
