9558c9642bca1edd1c76109c032d3d1d9de1f92b
ashish_mobitplus <ashish@intellikart.in>
2021-11-30 07:02:06 +0530

    filter api correction

--- controller/search.controller.js
+++ controller/search.controller.js
@@ -115,55 +115,84 @@ exports.getFilterResults = async (req, res, next) => {
   //   return Response(res, true, MESSAGE.RECORD_RETRIVED, getItemFromCache);
   // } else {
     let _filter_expression = "";
+    let _attribute_field = { };
+    let _attribute_value = { };
 
     if (_body.art_name != undefined) {
-      _filter_expression =
-        _filter_expression + ` and contains (art_name, ${_body.art_name}) `;
+        _filter_expression = _filter_expression + ' and contains (#f_art_name, :v_art_name) ';
+        _attribute_field["#f_art_name"] = "art_name";
+        _attribute_value[":v_art_name"] = _body.art_name;
     }
 
     if (_body.description != undefined) {
-      _filter_expression =
-        _filter_expression +
-        ` and contains (description, ${_body.description}) `;
+      _filter_expression = _filter_expression + ' and contains (#f_description, :v_description) ';
+      _attribute_field["#f_description"] = "description";
+      _attribute_value[":v_description"] = _body.description;
     }
 
     if (_body.title != undefined) {
-      _filter_expression =
-        _filter_expression + ` and contains (title, ${_body.title}) `;
+        _filter_expression = _filter_expression + ' and contains (#f_title, :v_title) ';
+        _attribute_field["#f_title"] = "title";
+        _attribute_value[":v_title"] = _body.title;
     }
 
     if (_body.artist != undefined) {
-      _filter_expression =
-        _filter_expression + ` and contains (artist, ${_body.artist}) `;
+      _filter_expression = _filter_expression + ' and contains (#f_artist, :v_artist) ';
+      _attribute_field["#f_artist"] = "artist";
+      _attribute_value[":v_artist"] = _body.artist;
     }
 
     if (_body.destination != undefined) {
-      _filter_expression =
-        _filter_expression +
-        ` and contains (destination, ${_body.destination}) `;
+      _filter_expression = _filter_expression + ' and contains (#f_destination, :v_destination) ';
+      _attribute_field["#f_destination"] = "destination";
+      _attribute_value[":v_destination"] = _body.destination;
     }
 
-    console.log('filter => ', _filter_expression);
+    if (_body.price != undefined) {
+      let _min_price = _body.price.min;
+      let _max_price = _body.price.max;
+
+      if (_min_price != undefined) {
+        _filter_expression = _filter_expression + ' and #f_price_min > :v_price_min ';
+        _attribute_field["#f_price_min"] = "price";
+        _attribute_value[":v_price_min"] = _min_price;
+      }
+
+      if (_max_price != undefined) 
+      {  
+        _filter_expression = _filter_expression + ' and #f_price_max < :v_price_max ';
+        _attribute_field["#f_price_max"] = "price";
+        _attribute_value[":v_price_max"] = _max_price;
+      }
+      
+    }
+
+    console.log('filter => ', _filter_expression, _attribute_field, _attribute_value);
 
     const docClient = new AWS.DynamoDB.DocumentClient();
     const params = {
       TableName: tables.IMAGES,
-      FilterExpression:
-        "(attribute_exists(labels." + _label + ") ) " + _filter_expression,
+      FilterExpression: "(attribute_exists(labels." + _label + ") ) " + _filter_expression ,
     };
+
+    if (_filter_expression.length > 0)
+    {
+      params.ExpressionAttributeNames = _attribute_field;
+      params.ExpressionAttributeValues = _attribute_value;
+    }
 
     docClient.scan(params, function (err, data) {
       if (err) {
         return Response(res, false, MESSAGE.RECORD_NOT_RETRIVED, err);
       } else {
         const { Items } = data;
-        for (let i = 0; i < Items.length; i++) {
-          if (Items[i].file_name !== undefined)
-            Items[i].url = config.awsS3BaseUrl + "" + Items[i].file_name ?? "";
-        }
-        Items.sort((item1, item2) => {
-          return compareObjects(item1, item2, _label);
-        });
+        // for (let i = 0; i < Items.length; i++) {
+        //   if (Items[i].file_name !== undefined)
+        //     Items[i].url = config.awsS3BaseUrl + "" + Items[i].file_name ?? "";
+        // }
+        // Items.sort((item1, item2) => {
+        //   return compareObjects(item1, item2, _label);
+        // });
 
         // cache.set(_label, Items);
         return Response(res, true, MESSAGE.RECORD_RETRIVED, Items);
